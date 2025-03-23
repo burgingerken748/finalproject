75b85931886f921eef3a05da5f3a9a8c7f08752d
burgingerken748 <burgingerken748@gmail.com>
2025-03-23 18:06:00 +0000

    Add 18.py

---
+++ src/18.py
@@ -0,0 +1,7 @@
+def calculate_expression(expression):
+    # Replace 'expression' with your actual expression here
+    result = eval(expression)
+    return result
+
+expression = "3 + 5 * 2 - (7 / 2)"
+print(calculate_expression(expression))
